1d8640a5a8b879ef787f5ca7e433cc7e1701858c
Mayur <rajsalve2022@gmail.com>
2025-12-23 12:39:02 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>App is New</h1>
+      </div>
+
+      <div>Mayur</div>
+    </>
+  );
+};
+
+export default App;
